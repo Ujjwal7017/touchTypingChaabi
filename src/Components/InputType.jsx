d9984cb01244ee471246658f17bea76c0d2889a0
Ujjwal Shukla <ujjwal7017@gmail.com>
2023-06-10 14:51:25 +0530

    files commited

---
+++ src/Components/InputType.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Random from "./Random";
+import Compares from "./Compares";
+
+function InputType() {
+  return (
+    <div>
+      <Random />
+      <Compares />
+    </div>
+  );
+}
+
+export default InputType;
